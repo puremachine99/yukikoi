279134690f700260ad23846fec424ba7d6f65905
unknown <puremachine99@gmail.com>
2025-03-26 22:32:35 +0700

    migration bugs fix

--- database/migrations/2024_09_05_052942_create_auctions_table.php
+++ database/migrations/2024_09_05_052942_create_auctions_table.php
@@ -3,32 +3,30 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('auctions', function (Blueprint $table) {
-            $table->string('auction_code')->primary(); // Primary key adalah auction_code
+            $table->string('auction_code')->primary();
             $table->string('title');
             $table->text('description');
-            $table->enum('jenis',['reguler','keeping contest','grow out','azukari'])->default('reguler');
+            $table->enum('jenis', ['reguler', 'keeping contest', 'grow out', 'azukari'])->default('reguler');
             $table->enum('status', ['draft', 'pending', 'rejected', 'ready', 'on going', 'completed'])->default('draft');
             $table->timestamp('start_time');
             $table->datetime('end_time')->nullable();
             $table->dateTime('contest_time')->nullable();
             $table->string('banner')->nullable();
-            $table->unsignedBigInteger('user_id'); // Foreign key ke users table
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
-            // Foreign key constraint
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('auctions');
+        Schema::enableForeignKeyConstraints();
     }
 };
